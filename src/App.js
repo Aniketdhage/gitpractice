b37c44c562746a1009f2697606fee8d6fd1586c6
Aniketdhage <aniketdhage90@gmail.com>
2022-04-27 12:55:56 +0530

    added component folder

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import GitComponent from "./GitFolderComp/GitComponent";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello ReactJS.</h1>
       <p>Practice Of Git Commands</p>
+      <GitComponent />
     </div>
   );
 }
