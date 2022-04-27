379e4be52e94286a188e71473d5229977232a0a0
Aniketdhage <aniketdhage90@gmail.com>
2022-04-27 13:59:54 +0530

    created test2 com/branch

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ function App() {
       <h1>Hello ReactJS.</h1>
       <p>Practice Of Git Commands</p>
       <GitComponent />
-      <p>this is from git test2 branch</p>
     </div>
   );
 }
---
+++ src/GitFolderComp/test2.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Test2 = () => {
+  return <div>this is from Test2 component/Branch</div>;
+};
+
+export default Test2;
